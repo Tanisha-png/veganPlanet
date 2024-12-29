1008b9c4007a61407dd417abf007b015f804cb42
Tanisha-png <user@email.com>
2024-12-29 16:05:09 -0500

    Add foods/new.ejs

--- controllers/foods.js
+++ controllers/foods.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user.js');
 
 const foods = ['vegan cheese', 'tofu'];
 
@@ -10,14 +11,19 @@ const ensureSignedIn = require('../middleware/ensure-signed-in');
 
 // GET /foods (index functionality) 
 router.get('/', (req, res) => {
-  res.send('Hooray - the foods are here!');
+  res.send('Foods listed here.');
 });
 
 // GET /foods/new (new functionality) 
 router.get('/new', ensureSignedIn, (req, res) => {
-  res.send('Add a food!');
+  res.render('foods/new.ejs', {title: 'Add Foods'});
 });
 
+// GET /foods/:foodId (show functionality)
+router.get('', () => {
+  
+});
 
+// POST /foods (create functionality)
 
 module.exports = router;
