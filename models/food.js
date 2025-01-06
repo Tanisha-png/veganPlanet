0732a0868c7413edb6ef08e5a7ea070ad406c5e8
Tanisha-png <user@email.com>
2025-01-06 16:51:10 -0500

    Add commentSchema to food model

--- models/food.js
+++ models/food.js
@@ -1,6 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const commentSchema = new mongoose.Schema({
+    user: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: false,
+    },
+});
+
+
 const foodSchema = new mongoose.Schema({
     veganName: {
         type: String,
@@ -14,7 +30,7 @@ const foodSchema = new mongoose.Schema({
         type: [Schema.Types.ObjectId],
         ref: 'User',
     },
-    // comments: [commentSchema],
+    comments: [commentSchema],
 });
 
 
